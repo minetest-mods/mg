986142cebf6887cbb126d4d43bb502023a9c151a
Sfan5 <sfan5@live.de>
2013-09-21 18:44:49 +0200

    Make village buildings look nicer

--- buildings.lua
+++ buildings.lua
@@ -4,6 +4,11 @@ local S = minetest.get_content_id("farming:soil_wet")
 local WH = minetest.get_content_id("farming:wheat_8")
 local A = minetest.get_content_id("air")
 local G = minetest.get_content_id("default:glass")
+local C = minetest.get_content_id("default:cobble")
+local T = minetest.get_content_id("default:tree")
+local WG = minetest.get_content_id("wool:grey")
+local FW = minetest.get_content_id("default:fence_wood")
+local TR = minetest.get_content_id("default:torch")
 local field = {
 	{
 		{S, WS, S, S, WS, S, S, WS, S},
@@ -40,44 +45,103 @@ local house = {
 		{W, W, W, W, W, W, W},
 	},
 	{
-		{W, W, W, A, W, W, W},
+		{T, W, W, A, W, W, T},
 		{W, A, A, A, A, A, W},
 		{W, A, A, A, A, A, W},
 		{W, A, A, A, A, A, W},
 		{W, A, A, A, A, A, W},
 		{W, A, A, A, A, A, W},
+		{T, W, W, W, W, W, T},
+	},
+	{
+		{T, C, C, A, C, C, T},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{T, C, G, G, G, C, T},
+	},
+	{
+		{T, C, C, C, C, C, T},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{T, C, G, G, G, C, T},
+	},
+	{
+		{T, C, C, C, C, C, T},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{C, A, A, A, A, A, C},
+		{T, C, C, C, C, C, T},
+	},
+	{
+		{W, W, W, W, W, W, W},
+		{W, W, W, W, W, W, W},
+		{W, W, W, W, W, W, W},
+		{W, W, W, W, W, W, W},
+		{W, W, W, W, W, W, W},
+		{W, W, W, W, W, W, W},
 		{W, W, W, W, W, W, W},
 	},
 	{
-		{W, W, W, A, W, W, W},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{G, A, A, A, A, A, G},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{W, W, G, G, G, W, W},
+		{A, A, A, A, A, A, A},
+		{A, W, W, W, W, W, A},
+		{A, W, A, A, A, W, A},
+		{A, W, A, A, A, W, A},
+		{A, W, A, A, A, W, A},
+		{A, W, W, W, W, W, A},
+		{A, A, A, A, A, A, A},
 	},
 	{
-		{W, W, W, W, W, W, W},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{W, A, A, A, A, A, W},
-		{W, W, W, W, W, W, W},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, W, W, W, A, A},
+		{A, A, W, A, W, A, A},
+		{A, A, W, W, W, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
 	},
 	{
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
-		{W, W, W, W, W, W, W},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, A, W, A, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
+		{A, A, A, A, A, A, A},
+	},
+}
+lamp = {
+	{
+		{A,  A, A},
+		{A, FW, A},
+		{A,  A, A},
+	},
+	{
+		{A,  A, A},
+		{A, FW, A},
+		{A,  A, A},
+	},
+	{
+		{A,  A, A},
+		{A, FW, A},
+		{A,  A, A},
+	},
+	{
+		{ A, TR,  A},
+		{TR, WG, TR},
+		{ A, TR,  A},
 	},
 }
 
 buildings = {
-	{sizex=3, sizez=3, ymin=0, ymax=4, scm=house},
-	{sizex=4, sizez=4, ymin=0, ymax=1, scm=field}
+	{sizex=3, sizez=3, ymin=0, ymax=8, scm=house},
+	{sizex=4, sizez=4, ymin=0, ymax=1, scm=field},
+	--{sizex=1, sizez=1, ymin=1, ymax=4, scm=lamp},
 }
